b7e64d97faae1a889afc662b60c4d7b37e361c8a
crqch <48185508+crqch@users.noreply.github.com>
2026-04-18 21:55:40 +0200

    feat(zad4): add time benchmarks

---
+++ fun/examples/fib_time.fun
@@ -0,0 +1,1 @@
+(funrec fib x -> if x == 0 then 1 else (if x == 1 then 1 else fib (x-1) + fib (x-2))) 40
---
+++ lista6_zad4/time/bin/main.ml
@@ -0,0 +1,4 @@
+let rec fib x = match x with 0 -> 1 | 1 -> 1 | w -> fib (w - 2) + fib (w - 1)
+
+let () =
+  () |> read_line |> int_of_string |> fib |> string_of_int |> print_endline
